b5e1a1c15c17c084c5b5808d6af70e9f428140ab
nkkize <nkk.snat@gmail.com>
2021-06-20 01:17:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# myreact
+
+react-redux demo app
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutPage = () => (
+  <div className="jumbotron">
+    <h1>About</h1>
+    <p>tech stack: react, redux</p>
+  </div>
+);
+
+export default AboutPage;
