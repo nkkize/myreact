5f2d544679ccfcb77e4e77aad85bc3ba0b789d80
nkkize <nkk.snat@gmail.com>
2021-06-23 01:21:05 +0530

    connect react redux

--- src/components/courses/coursesPage.js
+++ src/components/courses/coursesPage.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseAction from "../../redux/actions/courseAction";
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    let course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    debugger;
+    this.props.dispatch(courseAction.createCourse(this.state.course));
+    //alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Courses</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+
+        {this.props.courses.map((course) => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default CoursesPage;
+//this ensure dispatch is provided by connect
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+};
+
+function mapStateToProps(state, ownProps) {
+  debugger;
+  return {
+    courses: state.courses,
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,9 @@
+export default function courseReducer(state = [], action) {
+  debugger;
+  switch (action.type) {
+    case "CREATE_COURSE":
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
+}
